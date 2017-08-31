d6e735352c2703b5e16bde17fa2f65d9b069f6be
KongChan1988 <wangwei_198811@163.com>
2017-08-31 17:31:47 +0800

    笔记

--- Python_Study/第二模块学习/Day01/生成器.py
+++ Python_Study/第二模块学习/Day01/生成器.py
@@ -8,3 +8,23 @@ a = [1,2,3]
                                 for i in range(10):
                                     a.append(i*2)
 '''
+print('------斐波那契数列-----')
+def fib(max):
+    n,a,b = 0,0,1
+    while n<max:
+        yield b
+        a, b = b, a + b
+        n += 1
+    return 'done'
+f = fib(10)
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+
+
+
